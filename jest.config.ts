e60a7a81e39eda028db445d1448916dcb72f1ebd
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-01-20 12:03:55 -0300

    criando primeiros testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,11 @@
 export default {
-  jest: {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
 };
